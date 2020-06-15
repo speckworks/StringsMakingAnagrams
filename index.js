bc6f440b3446eb52bf9fd0fddbe32159742fb262
John K. Speck <speckworks@gmail.com>
2020-06-15 15:07:48 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+function makeAnagram(a, b) {
+    let deletions  = 0;
+//define process for iterating through string
+const process = (character) => {
+};
+    //compare two strings 
+for (let i = 0; i < a.length; i++) {
+    process(a[i]);
+    }
+    //if we have to delete a character deletions++
+}
+
+
+makeAnagram("hello", "hell");
